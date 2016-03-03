a9ef6c8da54806745889eb48d2de1018a7d3f9ce
Powen Ko <powenko@gmail.com>
2016-03-03 10:07:32 -0800

    add iOS code

---
+++ Sample12-Windows10IoT-RPi2-iOS-UART/iOS-UART/BTBLEiOS_TestCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTBLEiOS_TestCode
+//
+//  Created by Powen Ko on 7/28/14.
+//  Copyright (c) 2014 looptek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
